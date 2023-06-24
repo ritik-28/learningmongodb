c06d75de3d270d4a53994ce83f3e51dcdc5cb8aa
ritik-28 <tiwari.ritik.1.108@gmail.com>
2023-06-24 18:44:53 +0530

    done order

--- controllers/shop.js
+++ controllers/shop.js
@@ -101,14 +101,16 @@ exports.postOrder = (req, res, next) => {
   req.user
     .addOrder()
     .then((result) => {
+      return req.user.clearCart();
+    })
+    .then(() => {
       res.redirect("/orders");
     })
     .catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({ "user.userId": req.user._id })
     .then((orders) => {
       res.render("shop/orders", {
         path: "/orders",
